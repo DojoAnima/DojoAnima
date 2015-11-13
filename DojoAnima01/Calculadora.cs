1751591a0f16a10a3d975b9b32fa966f987bf354
Luiz Santos <lsantosdutra@gmail.com>
2015-11-13 15:31:41 -0200

    Commit Inicial

---
+++ DojoAnima01/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DojoAnima01
+{
+    public class Calculadora
+    {
+
+        public int Soma(int a, int b)
+        {
+            return 2;
+        }
+
+    }
+}
---
+++ DojoAnimaTest/CalculadoraTest.cs
@@ -0,0 +1,18 @@
+﻿using DojoAnima01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DojoAnimaTest
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        [TestMethod]
+        public void DeveSomar2()
+        {
+            var calculadora = new Calculadora();
+            var soma = calculadora.Soma(1, 1);
+
+            Assert.AreEqual(2, soma);
+        }
+    }
+}
