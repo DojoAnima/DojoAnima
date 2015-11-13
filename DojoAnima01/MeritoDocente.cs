73aaeb99a5e3d29832e670f7ec365c8ca26ea4dc
Luiz Santos <lsantosdutra@gmail.com>
2015-11-13 20:03:20 -0200

    Puzzle Melhor Professor

---
+++ DojoAnima01/MeritoDocente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DojoAnima01
+{
+    public class MeritoDocente
+    {
+        private readonly Dictionary<string, int> _quadroNotas;
+
+        public MeritoDocente()
+        {
+            _quadroNotas = new Dictionary<string, int>
+            {
+                {"Erix", 0},
+                {"Nande", 0},
+                {"Renato", 0}
+            };
+        }
+
+        public string MaiorNota()
+        {
+            var melhorProfessor = _quadroNotas.FirstOrDefault();
+            foreach (var quadroNota in _quadroNotas)
+            {
+                if (melhorProfessor.Value < quadroNota.Value)
+                {
+                    melhorProfessor = quadroNota;
+                }
+            }
+            return melhorProfessor.Key;
+        }
+
+        public void Votar(string professor, int nota)
+        {
+            if (nota < 0 || nota > 5)
+                throw new Exception("Nota deve ser maior ou igual a 0 e menor ou igual a 5");
+
+            var n = _quadroNotas[professor];
+            n = n + nota;
+
+            _quadroNotas[professor] = n;
+        }
+
+    }
+
+}
---
+++ DojoAnimaTest/MeritoDocenteTeste.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DojoAnima01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DojoAnimaTest
+{
+    [TestClass]
+    public class MeritoDocenteTeste
+    {
+        [TestMethod]
+        public void DeveObterProfessorMaisVotadoNoCasoSimples()
+        {
+           
+            var meritoDocente = new MeritoDocente();
+            var professor = meritoDocente.MaiorNota();
+            Assert.AreEqual("Erix",professor);
+        }
+        [TestMethod]
+        public void DeveObterProfessorMaisVotadoNoCasoVotacao()
+        {
+
+            var meritoDocente = new MeritoDocente();
+            meritoDocente.Votar("Nande",5);
+
+            
+            var professor = meritoDocente.MaiorNota();
+            Assert.AreEqual("Nande", professor);
+        }
+
+        
+
+    }
+
+
+}
